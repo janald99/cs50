9c0679a9687fbe8e13c2b23c8afa1fac98050470
janald_ho_from.intern <janald_ho_from.intern@cpf.gov.sg>
2023-11-28 17:24:39 +0800

    change debug=true and add 127.0.0.1 as allowed host for own deployment

--- project5/settings.py
+++ project5/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)l0-xw4az5g+e@j!u$&#n^n3ps=24r+s#et+jfnq=olr8hletf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
