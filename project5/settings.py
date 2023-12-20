704045ba6700b90b56a30394d9015c73272048c7
Janald Ho <janald99@hotmail.com>
2023-12-20 19:20:32 +0800

    fix settings.py

--- project5/settings.py
+++ project5/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://viewmew.s3.amazonaws.com/'
+
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
